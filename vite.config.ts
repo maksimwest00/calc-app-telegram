653dbdafe1b0a6e36c7e884ef173199e0160d23c
Максим Сухарев <m.sukharev@trekmark.ru>
2024-09-11 16:33:28 +0300

    настройка публикации на github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/calc-app-telegram",
   plugins: [react()],
   resolve: {
     alias: {
